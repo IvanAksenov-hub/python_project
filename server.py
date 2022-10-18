37ff50d01b7ee77fc85a5c81e894eaa05b60c6eb
admin <admin@admin>
2022-10-18 13:10:15 +0300

    test

--- server.py
+++ server.py
@@ -3,10 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/&lt;username&gt;", methods=['GET'])
-def index(username):
-    return "Hello %s" % username
-
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3333)
+    app.run(host='0.0.0.0')
