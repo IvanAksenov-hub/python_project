495810edb56aba0eaa43d267ba9cd3ba237428b8
admin <admin@admin>
2022-10-18 23:40:21 +0300

    +html +css

--- database_con.py
+++ database_con.py
@@ -11,11 +11,18 @@ con = psycopg2.connect(
 print("Database opened successfully")
 
 cur = con.cursor()
-postgres_insert_query = """ insert into scheme_1.users (man,status) values (%s,%s)"""
-record_to_insert = ('admin', 'active')
-cur.execute(postgres_insert_query,record_to_insert)
+#postgres_insert_query = """ insert into scheme_1.users (man,status) values (%s,%s)"""
+#record_to_insert = ('show', 'diactivated')
+#cur.execute(postgres_insert_query,record_to_insert)
+#con.commit()
 
-con.commit()
+sql_select_query = """select * from scheme_1.users"""
+cur.execute(sql_select_query)
+for row in cur:
+    print(row)
+
+
+
 print("Record inserted successfully")
 
 con.close()
--- server.py
+++ server.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 import psycopg2
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+def index():
+    return render_template('index.html')
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
