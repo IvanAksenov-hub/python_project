ca6f74a845e334979994a45daf8e33058fac50d5
hock <1@2.3>
2023-04-20 16:32:46 +0400

    correct

--- server.py
+++ server.py
@@ -3,23 +3,14 @@ import psycopg2
 
 app = Flask(__name__)
 
-def get_db_connection(): 
-    conn = psycopg2.connect(
-        database="work",
-        user="admin",
-        password="admin",
-        host="192.168.1.101",
-        port="5433"
-    )
-
 @app.route("/")
 def index():
-    connection = get_db_connection()
+    conn = psycopg2.connect("dbname=work user=admin password=admin host=192.168.1.101 port=5433")
+    cur = conn.cursor()
     sql_select_query = """select * from work.some_test"""
-    connection.close()
-    return render_template('index.html', records=sql_select_query)
-
-
+    records = cur.execute(sql_select_query)
+    conn.close()
+    return render_template('index.html', records=records)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
