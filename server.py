c3d2257cf442cc47fa8f00cd1f7e82eb7ba4b0fc
hock <1@2.3>
2023-04-21 16:54:33 +0400

    add delete button

--- server.py
+++ server.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import psycopg2
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
     conn = psycopg2.connect("dbname=work user=admin password=admin host=192.168.1.101 port=5433")
     cur = conn.cursor()
+    message_by_id = [('some text')]
+    message_id = ''
+    if request.method == 'POST':
+        try:
+            if request.form['action'] == 'insert_form':
+                message = request.form['message']
+                cur.execute("INSERT INTO work.some_test (message) VALUES (%s)", [message])
+                conn.commit()
+            elif request.form['action'] == 'id_form':
+                message_id = request.form['message_id']
+                cur.execute("SELECT message FROM work.some_test where id = (%s)", [message_id])
+                message_by_id = cur.fetchone()
+                conn.commit()
+            elif request.form['action'] == 'update_form':
+                message_id = int(request.form['message_id'])
+                update_message = str(request.form['update_message'])
+                if 'update_button' in request.form:
+                    cur.execute("UPDATE work.some_test SET message = %s WHERE id = %s", (update_message, message_id))
+                elif 'delete_button' in request.form:
+                    cur.execute("DELETE FROM work.some_test WHERE id = %s", (message_id,))
+                conn.commit()
+        except Exception as e:
+            print(e)
     sql_select_query = """select * from work.some_test order by id"""
     cur.execute(sql_select_query)
     rows = cur.fetchall()
@@ -23,7 +46,7 @@ def index():
             table_html += "<td>" + str(row[col_index]) + "</td>"
         table_html += "</tr>"
     table_html += "</tbody></table>"
-    return render_template('index.html', table_html=table_html)
+    return render_template('index.html', table_html=table_html, message_by_id=message_by_id, message_id=message_id)
 
 
 if __name__ == "__main__":
