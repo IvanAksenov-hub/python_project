e8f621d414a10e4fcfe64557fea699ac2bd9e9f3
hock <1@2.3>
2023-04-21 13:10:27 +0400

    correct

--- server.py
+++ server.py
@@ -1,26 +1,29 @@
 from flask import Flask, render_template
 import psycopg2
-import pandas as pd
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    records = []
     conn = psycopg2.connect("dbname=work user=admin password=admin host=192.168.1.101 port=5433")
     cur = conn.cursor()
-    sql_select_query = """select * from work.some_test"""
+    sql_select_query = """select * from work.some_test order by id"""
     cur.execute(sql_select_query)
-    for row in cur:
-        records.append(row)
-    conn.close()
-    df = pd.DataFrame.from_dict(records)
-    tables = df.to_html(index=False, classes='mystyle')
-    titles = ['id', 'message', 'date']
-    text_file = open("frame.html", "w")
-    text_file.write(tables)
-    text_file.close()
-    return render_template('index.html', titles=titles, tables=tables)
+    rows = cur.fetchall()
+    num_cols = len(cur.description)
+    table_html = "<table border="'1'" class="'tablestyle'" class="'dataframe'">"
+    table_html += "<thead><tr>"
+    for col in cur.description:
+        table_html += "<th>" + col.name + "</th>"
+    table_html += "</tr></thead>"
+    table_html += "<tbody>"
+    for row in rows:
+        table_html += "<tr>"
+        for col_index in range(num_cols):
+            table_html += "<td>" + str(row[col_index]) + "</td>"
+        table_html += "</tr>"
+    table_html += "</tbody></table>"
+    return render_template('index.html', table_html=table_html)
 
 
 if __name__ == "__main__":
