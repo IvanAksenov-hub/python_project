18cb6d640ffc1f952635776113a64ee140c3d2e8
hock <1@2.3>
2023-06-07 18:08:13 +0400

    add few pages add page with map from OpenStreet. Created possibility of mark on map and save coordinates in DB

--- server.py
+++ server.py
@@ -15,13 +15,58 @@ log_counter = Counter('logs_total', 'Total number of logs')
 log_buffer = io.StringIO()
 
 logger.add(log_buffer.write)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/map', methods=['GET', 'POST'])
+def map_page():
+    conn = psycopg2.connect("dbname=work user=admin password=admin host=192.168.100.101 port=5433")
+    cur = conn.cursor()
+    if request.method == 'POST':
+        try:
+            lat = float(request.form['lat'])
+            lng = float(request.form['lng'])
+            label = request.form['label']
+            cur.execute("INSERT INTO work.map_labels (latitude, longtitude, label) VALUES (%s, %s, %s)", (lat, lng, label))
+            conn.commit()
+            logger.info("insert in table work.map_labels - lat: {}, lng: {}, label: {}" .format(lat, lng, label))
+            print(lat)
+        except Exception as e:
+            print(e)
+    return render_template('map_page.html')
+
+@app.route('/map_labels', methods=['GET', 'POST'])
+def map_labels():
+    conn = psycopg2.connect("dbname=work user=admin password=admin host=192.168.100.101 port=5433")
+    cur = conn.cursor()
+    sql_select_query1 = """select * from work.map_labels order by id"""
+    cur.execute(sql_select_query1)
+    rows = cur.fetchall()
+    num_cols = len(cur.description)
+    table_map_labels_html = "<table border="'1'" class="'tablestyle'" class="'dataframe'">"
+    table_map_labels_html += "<thead><tr>"
+    # динамически создаваемая таблица для результата
+    for col in cur.description:
+        table_map_labels_html += "<th>" + col.name + "</th>"
+    table_map_labels_html += "</tr></thead>"
+    table_map_labels_html += "<tbody>"
+    for row in rows:
+        table_map_labels_html += "<tr>"
+        for col_index in range(num_cols):
+            table_map_labels_html += "<td>" + str(row[col_index]) + "</td>"
+        table_map_labels_html += "</tr>"
+    table_map_labels_html += "</tbody></table>"
+    return render_template('map_labels.html', table_map_labels_html=table_map_labels_html)
+
 @app.route('/log')
 def view_logs():
     logs = log_buffer.getvalue()
     return Response(logs, mimetype='text/plain')
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
+@app.route("/table", methods=['GET', 'POST'])
+def table():
     conn = psycopg2.connect("dbname=work user=admin password=admin host=192.168.100.101 port=5433")
     cur = conn.cursor()
     message_by_id = [('some text')]
@@ -68,8 +113,8 @@ def index():
             table_html += "<td>" + str(row[col_index]) + "</td>"
         table_html += "</tr>"
     table_html += "</tbody></table>"
-    return render_template('index.html', table_html=table_html, message_by_id=message_by_id, message_id=message_id)
+    return render_template('table.html', table_html=table_html, message_by_id=message_by_id, message_id=message_id)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='192.168.100.21')
